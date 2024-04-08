d2830227c0b61efeacbe1f7ff0e10932db3ea29f
Indiawhite <pdeval2706@gmail.com>
2024-04-08 20:49:27 +0100

    My first push to repo

---
+++ Programweek6/src/myhomework6/Number10.java
@@ -0,0 +1,22 @@
+package myhomework6;
+       //Write a Java program that takes two numbers as input and display the
+       // product of two numbers.
+       // Test Data:
+      //Input first number: 25
+      //Input second number: 5
+      //Expected Output : 25 x 5 = 125
+
+public class Number10 {
+        public static void main(String[] args){
+        //Input first number: 25
+        //Input second number: 5p
+
+        int a = 5;
+        int b = 25;
+
+        int result = a * b;
+
+            System.out.println("Expected output:"+result);
+    }
+}
+
