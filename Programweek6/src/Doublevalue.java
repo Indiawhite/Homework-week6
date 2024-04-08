dd524fa9b8de1d66734a9a9c82eec35ef47e7af1
Indiawhite <pdeval2706@gmail.com>
2024-04-08 22:46:12 +0100

    My first push to repo

---
+++ Programweek6/src/Doublevalue.java
@@ -0,0 +1,11 @@
+public class Doublevalue {
+    //Write a program to add 5 and 6.2, and print in console..
+
+    public static void main(String[] args) {
+        double num1 = 5;
+        double num2 = 6.2;
+        double sum = num1 + num2;
+
+        System.out.println("sum of num" + num1  + "and"  + num2 +  "=" + sum);
+    }
+}
