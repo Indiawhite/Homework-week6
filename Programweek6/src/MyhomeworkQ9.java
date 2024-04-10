35ebb14c58ed3a11948497af36497aaf4ada284e
Indiawhite <pdeval2706@gmail.com>
2024-04-11 00:00:42 +0100

    My first push to repo

---
+++ Programweek6/src/MyhomeworkQ9.java
@@ -0,0 +1,11 @@
+
+
+public class MyhomeworkQ9 {
+    //9. Write a program to convert the upper case to lower case.
+
+    public static void main(String[] args) {
+        String name = "Prem Patel";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+}
