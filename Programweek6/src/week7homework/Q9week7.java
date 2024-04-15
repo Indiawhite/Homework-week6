a600729869ea9b4dfbe4547bd25a80abf507b6ca
Indiawhite <pdeval2706@gmail.com>
2024-04-15 22:24:12 +0100

    My first push to repo

---
+++ Programweek6/src/week7homework/Q9week7.java
@@ -0,0 +1,13 @@
+package week7homework;
+//Write java program using for loop and print your group name 10 times.
+
+public class Q9week7 {
+    public static void main(String[] args) {
+        String name1 ="SoftwareGils";
+
+        for (int i = 1; i <11; i++){
+            System.out.println(i);
+            System.out.println(name1 );
+        }
+    }
+}
