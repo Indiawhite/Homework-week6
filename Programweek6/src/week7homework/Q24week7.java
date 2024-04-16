c429cb7b106c701f0af9ca36792242457c1b0fdb
Indiawhite <pdeval2706@gmail.com>
2024-04-17 00:14:16 +0100

    My first push to repo

---
+++ Programweek6/src/week7homework/Q24week7.java
@@ -0,0 +1,14 @@
+package week7homework;
+//Write java program using for loop and print surname 9 times.
+public class Q24week7 {
+    public static void main(String[] args) {
+        String surname = "Patel";
+
+        for (int i = 0; i < 9; i++){
+
+            System.out.println(surname);
+        }
+    }
+
+}
+
