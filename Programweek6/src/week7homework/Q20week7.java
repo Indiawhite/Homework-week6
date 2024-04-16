da4fa89ec00750341a651d7e2e4f15bdb4118885
Indiawhite <pdeval2706@gmail.com>
2024-04-16 23:41:07 +0100

    My first push to repo

---
+++ Programweek6/src/week7homework/Q20week7.java
@@ -0,0 +1,14 @@
+package week7homework;
+//Write a Java program to sort a numeric array and a string array.
+
+public class Q20week7 {
+    public static void main(String[] args) {
+
+        int [] numeric = {10,20,30,40,50};
+        String[] name = {"Prem" , "Megh" , "Mitul", "Deval"};
+
+        System.out.println(numeric[3]);
+        System.out.println(name[1]);
+
+    }
+}
