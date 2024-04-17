b8064a1b60ead8c42c49ab2e95b2e4cb35f290d8
Indiawhite <pdeval2706@gmail.com>
2024-04-17 21:56:02 +0100

    My first push to repo

---
+++ Programweek6/src/week7homework/Q25week7.java
@@ -0,0 +1,19 @@
+package week7homework;
+//Declare one method with return type and parameter and print your name in console.
+
+public class Q25week7 {
+    public static void main(String[] args) {
+        String myName = "Deval";
+        printName(myName);
+
+    }
+
+    public static void printName(String myName) {
+        System.out.println("name :" + myName  );
+
+
+    }
+
+
+}
+
