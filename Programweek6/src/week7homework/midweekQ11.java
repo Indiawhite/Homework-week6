7ae6a636803348c9901a6ff7984bd6f4af14e2f3
Indiawhite <pdeval2706@gmail.com>
2024-04-18 23:10:00 +0100

    My first push to repo

---
+++ Programweek6/src/week7homework/midweekQ11.java
@@ -0,0 +1,14 @@
+package week7homework;
+//Write a program to print a string entered by user.
+
+import java.util.Scanner;
+
+public class midweekQ11 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String output = scan.nextLine();
+
+        System.out.println(" String entered by User :" + output);
+
+    }
+}
