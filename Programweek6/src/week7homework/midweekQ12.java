32672384af52f880141b65fd3c74de0dfbb8285e
Indiawhite <pdeval2706@gmail.com>
2024-04-18 23:15:48 +0100

    My first push to repo

---
+++ Programweek6/src/week7homework/midweekQ12.java
@@ -0,0 +1,9 @@
+package week7homework;
+//Write a program to find the length of the string "refrigerator".
+
+public class midweekQ12 {
+    public static void main(String[] args) {
+        String name = "refrigerator";
+        System.out.println("Length of name :" + name.length());
+    }
+}
