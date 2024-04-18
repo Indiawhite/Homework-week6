db3b53ce3c3ff53691c26fc8c93060553d00058b
Indiawhite <pdeval2706@gmail.com>
2024-04-19 00:24:30 +0100

    My 2nd push to repo for Q9

--- Programweek6/src/week7homework/midweekQ8Q9.java
+++ Programweek6/src/week7homework/midweekQ8Q9.java
@@ -8,9 +8,8 @@ package week7homework;
 //Is student is allowed to sit in exam or not.
 
 import java.util.Scanner;
-import java.util.concurrent.SynchronousQueue;
 
-public class midweekQ8 {
+public class midweekQ8Q9 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
@@ -21,13 +20,25 @@ public class midweekQ8 {
         int attendedClasses = scan.nextInt();
 
         double attendedPercentage = (double) attendedClasses/totalClasses * 100;
-        System.out.println("AttendedPercentage :" + attendedPercentage );
+        System.out.println("AttendedPercentage :" + attendedPercentage+ "%" );
          if (attendedPercentage >= 75){
              System.out.println("Student allow to sit in exam");
          } else
              System.out.println("Student not allow to sit in exam");
 
+         // Q=9 allow student to sit if he/she has medical cause. Ask
+        //user if he/she has medical cause or not ( 'Y' or 'N' ) and print accordingly.
 
+        if (attendedPercentage >=75){
+            System.out.println("Student allow to sit in exam");
+        } else
+            System.out.println("Student have medical condition Y/N ?");
+           char medicalCondition = scan.next().charAt(0);
+
+           if (medicalCondition == 'Y' || medicalCondition == 'y'){
+               System.out.println(" Student allow sit in exam with medicalCondition");
+        } else
+               System.out.println(" Student not allow sit in exam ");
 
     }
 }
